de7e549e644d6fb988292003909e18b7aef9e859
Josh Rothenberg <jfrothen@uvm.edu>
2019-08-22 09:43:03 -0400

    Add files via upload

---
+++ hints.js
@@ -0,0 +1,9 @@
+const hints = {
+    cant_reach: "Try changing the gravity!",
+    jump_map: "What if it were . . . bouncier? (Can confirm it's possible as is however)",
+    demo_map: "Use the arrow keys to move! ps - Don't fall in the lava.",
+    lava_map: "What if nothing could kill you? What an interesting idea . . .",
+    flappy_bird: "Zero gravity is sort of like being a bird, right?",
+    the_maze: "Not much of a maze if I can't prevent you from looking at the map I guess",
+    the_block: "Never forget that the map is whatever you want it to be, man"
+};
